a3a60ca9b98fa8679efc07a5938dea2456524f4c
Konrad Guzior <Guzior@Konrad.local>
2020-10-15 16:24:45 +0200

    Rerouting serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App';
-import { store } from './store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './Components/setup/serviceWorker';
-import {SocketService} from './service/SocketService'
-import {ChatContext} from './context/ChatContext'
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './Components/App/App'
+import { store } from './store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './service/serviceWorker'
 
 ReactDOM.render(
       <Provider store={store}>
@@ -16,9 +12,6 @@ ReactDOM.render(
       </Provider>
     ,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
