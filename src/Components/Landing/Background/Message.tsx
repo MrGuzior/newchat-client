4ba63b2fb4bb1635eb4533423094b4d947df33ac
Konrad Guzior <Guzior@Konrad.local>
2020-10-16 11:29:07 +0200

    Experimenting with framer-motion

--- src/Components/Landing/Background/Message.tsx
+++ src/Components/Landing/Background/Message.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
 import './Message.css'
 
+
 const Message = () => {
+
+    console.log(window.innerHeight)
+
+    const messageAnimate = {
+        y: [500,500,400,400,300,300,200,200,100,100,0,0]
+    }
+
+    const messageInitial = {
+        y: 600
+    }
+
+    const messageTransition = {
+        duration: 8,
+        type: 'spring',
+        stiffness: 50,
+        repeat: Infinity
+    }
+
+    const h2Variables = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1
+        }
+    }
+
+
     return (
-        <div className='message'>
-            <p>Mock message</p>
-        </div>
+        <motion.div animate={messageAnimate} initial={messageInitial} transition={messageTransition} className='message'>
+            <motion.h2 variants={h2Variables} initial='hidden' animate='visible'>Mock message</motion.h2>
+        </motion.div>
     )
 }
 
